d7c1b52cbf4a591d746d8aadb14989c6a20fcf07
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-14 14:17:12 -0300

    criando primeiro objeto e transformando em json, criando segunda rota usando flask

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+purchase_order = [
+    {
+        'id': 1,
+        'description': 'Pedido de compra 1',
+        'items': [
+            {
+                'id': 1,
+                'description': 'Item do pedido 1',
+                'price': 10.99,
+            },
+            {
+                'id': 2,
+                'description': 'Item do pedido 2',
+                'price': 100.99,
+            }
+        ],
+    },
+]
 
 
 @app.route('/')
 def home():
     return 'Hello world'
+
+
+@app.route('/purchase_orders')
+def get_purchase_order():
+    return jsonify(purchase_order)
 
 
 app.run(port=5000)
