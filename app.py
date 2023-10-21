e8433d659fc960a8a21106c11b51ca1e816c9cbc
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-21 12:15:15 -0300

    inicio do projeto

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from flask import Flask
 from flask_restful import Api
 
-from purchase_orders.resources import PurchaseOrders, PurchaseOrdersById
 
-from purchase_orders_items.resources import PurchaseOrderItems
+def create_app():
+    app = Flask(__name__)
+    api = Api(app)
 
-app = Flask(__name__)
-api = Api(app)
+    app.run(port=5000)
 
-
-api.add_resource(PurchaseOrders, '/purchase_orders')
-api.add_resource(PurchaseOrdersById, '/purchase_orders/<int:id>')
-
-api.add_resource(PurchaseOrderItems, '/purchase_orders/<int:id>/items')
-
-app.run(port=5000)
+    return app
