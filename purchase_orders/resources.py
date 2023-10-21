552bf0c5b60d7badf67af8391f560fa7f5efd509
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-21 14:34:32 -0300

    test: criando testes para metodos get e post no /purchase_orders

--- purchase_orders/resources.py
+++ purchase_orders/resources.py
@@ -1,9 +1,36 @@
 from flask import jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from purchase_object import purchase_order_obj
 
 
 class PurchaseOrders(Resource):
+    parser = reqparse.RequestParser()
+
+    parser.add_argument(
+        'id',
+        type=int,
+        required=True,
+        help='Verifique o ID.'
+    )
+
+    parser.add_argument(
+        'description',
+        type=str,
+        required=True,
+        help='Informe uma descrição.'
+    )
 
     def get(self):
         return jsonify(purchase_order_obj)
+
+    def post(self):
+        request_data = PurchaseOrders().parser.parse_args()
+        purchase_order = {
+            'id': request_data['id'],
+            'description': request_data['description'],
+            'items': []
+        }
+
+        purchase_order_obj.append(purchase_order)
+
+        return jsonify(purchase_order)
--- test/purchase_orders/test_resources.py
+++ test/purchase_orders/test_resources.py
@@ -1,3 +1,6 @@
+import json
+
+
 def test_get_purchase_orders(test_client):
     response = test_client.get('/purchase_orders')
 
@@ -5,3 +8,22 @@ def test_get_purchase_orders(test_client):
     assert response.json[0]['id'] == 1
     assert response.json[0]['description'] == 'Pedido de compra 1'
     assert len(response.json[0]['items']) == 2
+
+
+def test_post_purchase_orders(test_client):
+    obj = {
+        'id': 2,
+        'description': 'Pedido de compra 2',
+        'items': [],
+    }
+
+    response = test_client.post(
+        '/purchase_orders',
+        data=json.dumps(obj),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 200
+    assert response.json['id'] == obj['id']
+    assert response.json['description'] == obj['description']
+    assert response.json['items'] == []
