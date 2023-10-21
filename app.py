3f757be29b135287d0308a5682fb5058e100cbbc
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-21 18:04:14 -0300

    adicionando ultima funcionalidad do projeto(get add item to a purchase order)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from purchase_orders.resources import PurchaseOrders, PurchaseOrdersById
+from purchase_orders_items.resources import PurchaseOrderItems
 
 
 def create_app():
@@ -9,5 +10,6 @@ def create_app():
 
     api.add_resource(PurchaseOrders, '/purchase_orders')
     api.add_resource(PurchaseOrdersById, '/purchase_orders/<int:id>')
+    api.add_resource(PurchaseOrderItems, '/purchase_orders/<int:id>/items')
 
     return app
