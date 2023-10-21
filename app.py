582ed14e4e909c2fb2eb0cafdb4c8a30df733f10
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-21 11:17:32 -0300

    criando o get para o pegar pedidos por id e adidionando rota no app

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 
-from purchase_orders.resources import PurchaseOrders
+from purchase_orders.resources import PurchaseOrders, PurchaseOrdersById
 
 app = Flask(__name__)
 api = Api(app)
 
 
 api.add_resource(PurchaseOrders, '/purchase_orders')
+api.add_resource(PurchaseOrdersById, '/purchase_orders/<int:id>')
 
 app.run(port=5000)
--- purchase_orders/resources.py
+++ purchase_orders/resources.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 
 purchase_orders = [
@@ -23,5 +23,50 @@ purchase_orders = [
 
 
 class PurchaseOrders(Resource):
+    parser = reqparse.RequestParser()
+
+    parser.add_argument(
+        'id',
+        type=int,
+        required=True,
+        help='Informe um ID válido.'
+    )
+
+    parser.add_argument(
+        'description',
+        type=str,
+        required=True,
+        help='Informe uma descrição.'
+    )
+
     def get(self):
         return jsonify(purchase_orders)
+
+    def post(self):
+        request_data = PurchaseOrders().parser.parse_args()
+        purchase_order = {
+            'id': request_data['id'],
+            'description': request_data['description'],
+            'items': []
+        }
+
+        purchase_orders.append(purchase_order)
+
+        return jsonify(purchase_order)
+
+
+class PurchaseOrdersById(Resource):
+    parser = reqparse.RequestParser()
+
+    parser.add_argument(
+        'id',
+        type=int,
+        required=True,
+        help='Informe um ID.'
+    )
+
+    def get(self, id):
+        for purchase_order in purchase_orders:
+            if purchase_order['id'] == id:
+                return jsonify(purchase_order)
+        return jsonify({'message': f'pedido {id} nao encontrado.'})
