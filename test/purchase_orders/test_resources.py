e55608f21606937de2cbd104463fec6a354b553c
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-22 11:21:40 -0300

    test: adicionando teste para cenários de falha

--- test/purchase_orders/test_resources.py
+++ test/purchase_orders/test_resources.py
@@ -29,6 +29,34 @@ def test_post_purchase_orders(test_client):
     assert response.json['items'] == []
 
 
+def test_post_empty_id(test_client):
+    obj = {
+        'description': 'Pedido de compra 2',
+    }
+
+    response = test_client.post(
+        '/purchase_orders',
+        data=json.dumps(obj),
+        content_type='application/json'
+    )
+
+    assert response.json['message']['id'] == 'Verifique o ID.'
+
+
+def test_post_empty_description(test_client):
+    obj = {
+        'id': 10
+    }
+
+    response = test_client.post(
+        '/purchase_orders',
+        data=json.dumps(obj),
+        content_type='application/json'
+    )
+
+    assert response.json['message']['description'] == 'Informe uma descrição.'
+
+
 def test_get_purchase_order_by_id(test_client):
     response = test_client.get('/purchase_orders/1')
 
