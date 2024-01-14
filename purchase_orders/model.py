547974901d847fddf0b5802c2987c3f762a89327
welli7ngton <wellingtonasilva45@gmail.com>
2024-01-14 16:08:54 -0300

    métodos purchase order model

--- purchase_orders/model.py
+++ purchase_orders/model.py
@@ -1,3 +1,4 @@
+# flake8:noqa
 from db import DB
 
 
@@ -9,3 +10,20 @@ class PurchaseOrderModel(DB.Model):
 
     def __init__(self, description) -> None:
         self.description = description
+    
+    def as_dict(self):
+        return {
+            c.name: getattr(self, c.name) for c in self.__table__.columns
+        }
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all() # select * from purchase_order
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first() # select * from purchase_order where id == _id
+
+    def save(self):
+        DB.session.add(self)
+        DB.session.commit()
