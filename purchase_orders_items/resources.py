327585625c13ab7818a0d9793967069ae87f0aa5
welli7ngton <wellingtonasilva45@gmail.com>
2024-01-15 13:58:49 -0300

    apagando os dados do banco pruchase_order_items depois de cada teste

--- purchase_orders_items/resources.py
+++ purchase_orders_items/resources.py
@@ -23,9 +23,11 @@ class PurchaseOrdersItems(Resource):
     )
 
     def get(self, id):
-        purchase_orders_items = PurchaseOrdersItemsModel.find_by_purchase_order_id(id)
-
-        return [p.as_dict() for p in purchase_orders_items]
+        purchase_order = PurchaseOrderModel.find_by_id(id)
+        if purchase_order:
+            purchase_orders_items = PurchaseOrdersItemsModel.find_by_purchase_order_id(id)
+            return [p.as_dict() for p in purchase_orders_items]
+        return jsonify({'message': f'itens do pedido {id} nao encontrados.'})
 
     def post(self, id):
         purchase_order = PurchaseOrderModel.find_by_id(id)
