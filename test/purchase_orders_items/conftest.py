09d0ac262a9c00b4e435f5fded3ce58f147d262c
welli7ngton <wellingtonasilva45@gmail.com>
2024-01-22 12:50:42 -0300

    refatorando testes

--- test/purchase_orders_items/conftest.py
+++ test/purchase_orders_items/conftest.py
@@ -7,7 +7,7 @@ from purchase_orders.model import PurchaseOrderModel
 
 @pytest.fixture()
 def seed_db():
-    po = PurchaseOrderModel('Pedido de testes')
+    po = PurchaseOrderModel('Pedido de testes', 50)
     DB.session.add(po)
     DB.session.commit()
 
