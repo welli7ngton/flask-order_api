c9a8be947a023aa70501b27ba9d461c8e8efbe02
welli7ngton <wellingtonasilva45@gmail.com>
2024-02-05 12:30:49 -0300

    configurando jwt manager

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ from purchase_orders.resources import PurchaseOrders, PurchaseOrdersById
 from purchase_orders_items.resources import PurchaseOrdersItems
 from db import DB
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 
 def create_app():
     app = Flask(__name__)
     api = Api(app)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URI_TEST']
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URI']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 
     DB.init_app(app)
     Migrate(app, DB)
